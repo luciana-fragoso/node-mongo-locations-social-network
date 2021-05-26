9a1ab5eff7e167f76b1ec1ebd0fabb719a8e8ff0
Luciana <luhciana@gmail.com>
2021-05-26 10:42:42 +0100

    Testing

--- routes/home.js
+++ routes/home.js
@@ -9,7 +9,7 @@ router.get('/signup',homeController.signup);
 router.get('/myProfile',homeController.myProfile);
 router.get('/logout',homeController.logout);
 
-router.get('/seeLocation/:id',locationController.seeLocation);
+
 router.get('/newLocation',locationController.newLocation);
 router.get('/searchLocation/:searchInput',locationController.searchLocation);
 
